19a2ed3f5d23f68318e8392b7a29438886879b08
drumato <drumato43@gmail.com>
2025-01-20 22:45:53 +0900

    implement simple reconcile
    Signed-off-by: drumato <drumato43@gmail.com>

--- internal/controller/secret_controller.go
+++ internal/controller/secret_controller.go
@@ -18,6 +18,10 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
@@ -53,22 +57,42 @@ func (r *SecretReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	logger.V(9).Info("start reconciliation")
 
 	secret := corev1.Secret{}
-	if err := r.Client.Get(ctx, req.NamespacedName, &secret); err != nil && !apierrors.IsNotFound(err) {
+	if err := r.Client.Get(ctx, req.NamespacedName, &secret); err != nil {
+		if !apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
 		return ctrl.Result{}, err
 	}
 
 	annotations := secret.GetAnnotations()
 
 	var ttl time.Time
-	ttlAnnotation := annotations[TTLAnnotation]
-	if ttlAnnotation != "" {
+	switch {
+	case annotations[TTLAfterAgeAnnotation] != "":
+		d, err := parseDuration(annotations[TTLAfterAgeAnnotation])
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		ttl = secret.GetCreationTimestamp().UTC().Add(d)
+	case annotations[TTLAnnotation] != "":
 		layout := time.RFC3339
-		if l := 
+		if l := annotations[TTLLayoutAnnotation]; l != "" {
+			layout = convertLayout(l)
+		}
+		t, err := time.Parse(layout, annotations[TTLAnnotation])
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		ttl = t.UTC()
+		logger.V(0).Info(ttl.String())
+	default:
+		return ctrl.Result{}, fmt.Errorf("'ttlsecret.drumato.com/ttl' or 'ttlsecret.drumato.com/ttl-after-age' must be specified")
 	}
 
-	if time.Now().Before(ttl) {
+	now := time.Now().UTC()
+	if now.Before(ttl) {
 		logger.V(0).Info("will enqueue after ttl will be expired")
-		return ctrl.Result{RequeueAfter: time.Until(ttl)}, nil
+		return ctrl.Result{RequeueAfter: ttl.Sub(now)}, nil
 	}
 
 	logger.V(0).Info("delete secret", "namespace", secret.GetNamespace(), "name", secret.GetName())
@@ -77,6 +101,61 @@ func (r *SecretReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func convertLayout(l string) string {
+	switch strings.ToLower(l) {
+	case "rfc822":
+		return time.RFC822
+	case "rfc822z":
+		return time.RFC822Z
+	case "rfc1123":
+		return time.RFC1123
+	case "rfc1123z":
+		return time.RFC1123Z
+	case "kitchen":
+		return time.Kitchen
+	case "rfc3339":
+		return time.RFC3339
+	case "rfc3339nano":
+		return time.RFC3339Nano
+	case "datetime":
+		return time.DateTime
+	case "dateonly":
+		return time.DateOnly
+	case "timeonly":
+		return time.TimeOnly
+	default:
+		return l
+	}
+}
+
+var reDuration = regexp.MustCompile(`^(\d+)([dhms])$`)
+
+// parseDuration parses the duration representation like (3d/2h/15m/10s).
+func parseDuration(s string) (time.Duration, error) {
+	match := reDuration.FindStringSubmatch(s)
+	if match == nil {
+		return 0, fmt.Errorf("invalid duration format: %s", s)
+	}
+
+	value, err := strconv.Atoi(match[1])
+	if err != nil {
+		return 0, fmt.Errorf("invalid number: %w", err)
+	}
+
+	switch match[2] {
+	case "d":
+		return time.Duration(value) * 24 * time.Hour, nil
+	case "h":
+		return time.Duration(value) * time.Hour, nil
+	case "m":
+		return time.Duration(value) * time.Minute, nil
+	case "s":
+		return time.Duration(value) * time.Second, nil
+	default:
+		return 0, fmt.Errorf("unknown duration unit: %s", match[2])
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
--- internal/controller/secret_controller_test.go
+++ internal/controller/secret_controller_test.go
@@ -17,42 +17,134 @@ limitations under the License.
 package controller
 
 import (
+	"log"
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 var _ = Describe("Secret Controller", func() {
-	Context("When ttl was speficied", func() {
-		It("should successfully reconcile the resource", func() {
-			const secretName = "secretcontroller-ttl-succeed"
-			secret := corev1.Secret{
-				ObjectMeta: metav1.ObjectMeta{
-					Namespace: "default",
-					Name:      secretName,
-					Annotations: map[string]string{
-						TTLAnnotation: "",
+	Context("Reconcile", func() {
+		Context("When ttl was speficied", func() {
+			It("should successfully delete the secret", func() {
+				const secretName = "secretcontroller-ttl-succeed"
+				req := ctrl.Request{
+					NamespacedName: types.NamespacedName{
+						Namespace: "default",
+						Name:      secretName,
 					},
-				},
-			}
-			Eventually(func() error {
-				return k8sClient.Create(ctx, &secret)
-			}).Should(Succeed())
+				}
+				now := time.Now().UTC()
+				secret := corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "default",
+						Name:      secretName,
+						Annotations: map[string]string{
+							TTLAnnotation:       now.Add(2 * time.Second).Format(time.DateTime),
+							TTLLayoutAnnotation: "datetime",
+						},
+					},
+				}
+				Eventually(func() error {
+					return k8sClient.Create(ctx, &secret)
+				}).WithTimeout(30 * time.Second).Should(Succeed())
 
-			reconciler := &SecretReconciler{Client: k8sClient}
-			reconciler.Reconcile(ctx, ctrl.Request{
-				NamespacedName: types.NamespacedName{
-					Namespace: "default",
-					Name:      secretName,
-				},
+				reconciler := &SecretReconciler{Client: k8sClient}
+
+				By("checking the secret resource is actually deleted")
+				Eventually(func() bool {
+					_, err := reconciler.Reconcile(ctx, req)
+					if err != nil {
+						log.Println(err)
+						return false
+					}
+					err = k8sClient.Get(ctx, req.NamespacedName, &secret)
+					return apierrors.IsNotFound(err)
+				}).WithTimeout(30 * time.Second).Should(BeTrue())
 			})
+		})
 
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		Context("When ttl-after-age was speficied", func() {
+			It("should successfully delete the secret", func() {
+				const secretName = "secretcontroller-ttl-succeed"
+				req := ctrl.Request{
+					NamespacedName: types.NamespacedName{
+						Namespace: "default",
+						Name:      secretName,
+					},
+				}
+				secret := corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "default",
+						Name:      secretName,
+						Annotations: map[string]string{
+							TTLAfterAgeAnnotation: "1s",
+						},
+					},
+				}
+				Eventually(func() error {
+					return k8sClient.Create(ctx, &secret)
+				}).WithTimeout(30 * time.Second).Should(Succeed())
+
+				reconciler := &SecretReconciler{Client: k8sClient}
+
+				By("checking the secret resource is actually deleted")
+				Eventually(func() bool {
+					_, err := reconciler.Reconcile(ctx, req)
+					if err != nil {
+						log.Println(err)
+						return false
+					}
+					err = k8sClient.Get(ctx, req.NamespacedName, &secret)
+					return apierrors.IsNotFound(err)
+				}).WithTimeout(30 * time.Second).Should(BeTrue())
+			})
+		})
+	})
+
+	Context("parseDuration", func() {
+		Context("3d", func() {
+			It("shoud succeed", func() {
+				d, err := parseDuration("3d")
+				Expect(err).Should(Succeed())
+
+				Expect(d).Should(BeIdenticalTo(3 * 24 * time.Hour))
+
+			})
+		})
+
+		Context("4h", func() {
+			It("shoud succeed", func() {
+				d, err := parseDuration("4h")
+				Expect(err).Should(Succeed())
+
+				Expect(d).Should(BeIdenticalTo(4 * time.Hour))
+			})
+		})
+
+		Context("5m", func() {
+			It("shoud succeed", func() {
+				d, err := parseDuration("5m")
+				Expect(err).Should(Succeed())
+
+				Expect(d).Should(BeIdenticalTo(5 * time.Minute))
+			})
+		})
+
+		Context("6s", func() {
+			It("shoud succeed", func() {
+				d, err := parseDuration("6s")
+				Expect(err).Should(Succeed())
+
+				Expect(d).Should(BeIdenticalTo(6 * time.Second))
+			})
 		})
 	})
 })
